e61b2a2e5ad7d3a3456c941b82742ea7cf503325
shehbaazsk <shehbaazwebdev@gmail.com>
2024-12-20 21:17:20 +0530

    add super user func

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -10,7 +10,17 @@ class UserManager(BaseUserManager):
         if not password:
             raise ValueError("User must have a password")
 
-        user = self.model(email=self.normalize_email(email.lower()), **extra_fields)
+        user = self.model(email=self.normalize_email(
+            email.lower()), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
         return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with admin privileges.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
